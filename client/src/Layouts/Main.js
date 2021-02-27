4be4bb348b69efdf5c35063e421248e0ce635702
juliandito <68038701+juliandito@users.noreply.github.com>
2021-02-28 00:33:23 +0700

    merge bintangz

--- client/src/Layouts/Main.js
+++ client/src/Layouts/Main.js
@@ -4,11 +4,13 @@ import './Main.css';
 import HowTo from '../Components/HowTo';
 import EventList from '../Components/EventList';
 
+import GameBoard from '../Components/game-board/GameBoard';
+
 const Main = () =>{
     return(<>
         <div className="container-fluid back">
+            <GameBoard />
             <EventList date="1 January 2001" />
-            {/* <HowTo /> */}
         </div>
     </>);
 }
