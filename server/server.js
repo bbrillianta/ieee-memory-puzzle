3426d1e13da41356e537464603c5168962e65461
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-02-27 12:44:10 +0700

    Setup db

--- server/server.js
+++ server/server.js
@@ -7,17 +7,42 @@ const cors = require('cors');
 
 //Basic configuration
 const app = express();
-const mongoConn = null;
+const mongoConn = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Mongoose setup
+const { Schema } = mongoose;
+const dateSchema = new Schema({
+    name: String,
+    date: String,
+    desc: String
+});
+
+const Date = mongoose.model('Date', dateSchema, 'dates');
 
 //Routing
 app.get('/', (req, res) => {
     res.json({});
 });
 
+app.post('/store', async (req, res) => {
+    const { name, date, desc } = req.body;
+    console.log(name, date, desc);
+    const doc = await new Date({
+        name,
+        date,
+        desc
+    });
+
+    const storedDoc = await doc.save();
+
+    res.json({ success: true, storedDoc });
+});
+
 //Starting server
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+    console.log(`Example app listening at http://localhost:${process.env.PORT || 3001}`);
 });
