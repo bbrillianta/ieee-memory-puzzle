1e4b61149b67abf3256721d05fe70ddd63d812c2
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-02-27 23:46:03 +0700

    Component game jadi

--- client/src/components/game-board/GameBoard.js
+++ client/src/components/game-board/GameBoard.js
@@ -4,11 +4,22 @@ import Button from '../button/Button.js';
 const GameBoard = () => {
     return (
         <div className="game-board container">
-            <div className="row">
-                <div className="col-md-6">Life</div>
-                <div className="col-md-6 text-right">Points</div>
+            <div className="row pt-5">
+                <div className="col-md-6">
+                    <p className="ml-5">
+                        Life: 
+                        <img className="ml-2" src="./Vector.png"></img>
+                        <img className="ml-1" src="./Vector.png"></img>
+                        <img className="ml-1"   src="./Vector.png"></img>
+                    </p> 
+                </div>
+                <div className="col-md-6 text-right">
+                    <p className="mr-5">
+                        Points: 0
+                    </p>
+                </div>
             </div>
-            <div className="row question-board py-2">
+            <div className="row question-board py-3">
                 <div className="col text-center question-text">1 January 2021</div>
             </div>
             <div className="row pt-5 pb-4">
