b404b72e6f4291224f6df463a55d3ad7eb312c4c
juliandito <68038701+juliandito@users.noreply.github.com>
2021-02-27 20:24:15 +0700

    main/bg component

---
+++ client/src/Layouts/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () =>{
+    return(<>
+        <div className="back">
+            Tes
+        </div>
+    </>)
+}
+
+export default Main;
