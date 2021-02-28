0acc84595d66840463cea3cbe7d9da9dd2e70889
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-02-28 10:05:01 +0700

    Game done

--- client/src/Components/how_to/HowTo.js
+++ client/src/Components/how_to/HowTo.js
@@ -6,13 +6,12 @@ const HowTo = () =>{
         <div className="cont-how">
             <div className="how">
                 <h3 className="d-flex justify-content-center">How to Play</h3>
-                <p>A Date (ex: 1 January 2001) will be shown to you, one at a time. If you’ve found that date 
-                    more than once, click FOUND and if it’s a new word click NEW. If you make a wrong choice,
+                <p>A Date (ex: 1 January 2001) will be shown to you, one at a time. If you’ve <strong>found</strong> that date 
+                    <strong> more than once</strong> during the game, click <strong>FOUND</strong> and if it’s a <strong>new word</strong>, click <strong>NEW</strong>. If you make a wrong choice,
                     you will lose one life point, and if you lose all of your life point the game will be over.
                 </p>
                 <p id="akhir">
-                    Note that every time you found a new date, you can see a trivial information about some 
-                    important events that happened on that date. HAVE FUN!!
+                    Note that we provide trivial information about important events that happened on each date so that you can play and learn history at the same time. You can see that information after you click FOUND or NEW, Have Fun !
                 </p>
             </div>
         </div>
